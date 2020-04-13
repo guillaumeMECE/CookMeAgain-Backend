51241b7f2947d41e15d35c0714fcf97f6170efaf
Guillaume <guillaume.maurin@edu.ece.fr>
2020-04-13 20:12:08 +0200

    Scrapper get quantity and Ingredients

--- backend/routes/controllers/scrapper/ScrappingFromUrl.js
+++ backend/routes/controllers/scrapper/ScrappingFromUrl.js
@@ -47,9 +47,30 @@ const process = async (inputs) => {
 
         // TODO Remove invisible char
         const lengthOf_rawPreparationList = $('.recipe-preparation__list__item', html).length; // Preparation List of the recipe
-        output.preparation_list=[];
+        output.preparation_list = [];
         for (let id = 0; id < lengthOf_rawPreparationList; id++) {
             output.preparation_list.push($('.recipe-preparation__list__item', html)[id].children[0].data)
+        }
+
+        const lengthOf_rawIngredientQt = $('.recipe-ingredient-qt', html).length; // Preparation List of the recipe
+        const lengthOf_rawIngredient = $('.ingredient', html).length; // Preparation List of the recipe
+        console.log(lengthOf_rawIngredientQt, " = ", lengthOf_rawIngredient);
+        output.ingredients_list = [];
+        if (lengthOf_rawIngredientQt === lengthOf_rawIngredient) {
+            let rawQt;
+            for (let id = 0; id < lengthOf_rawIngredientQt; id++) {
+                if ($('.recipe-ingredient-qt', html)[id].children[0] === undefined) {
+                    rawQt = "0";
+
+                } else {
+                    rawQt = $('.recipe-ingredient-qt', html)[id].children[0].data;
+                }
+                output.ingredients_list.push(
+                    {
+                        qt: rawQt,
+                        ingredient: $('.ingredient', html)[id].children[0].data
+                    });
+            }
         }
 
         return output;
@@ -74,3 +95,84 @@ const scrappingFromUrl = async (req, res) => {
 };
 
 module.exports = scrappingFromUrl;
+
+
+// {
+//     type: 'text',
+//         data: '\n\t\t\t\t\t',
+//             parent: {
+//         type: 'tag',
+//             name: 'li',
+//                 namespace: 'http://www.w3.org/1999/xhtml',
+//                     attribs: [Object: null prototype] {
+//             class: 'recipe-ingredients__list__item'
+//         },
+//         'x-attribsNamespace': [Object: null prototype] { class: undefined },
+//         'x-attribsPrefix': [Object: null prototype] { class: undefined },
+//         children: [[Circular], [Object], [Object], [Object]],
+//             parent: {
+//             type: 'tag',
+//                 name: 'ul',
+//                     namespace: 'http://www.w3.org/1999/xhtml',
+//                         attribs: [Object: null prototype],
+//                             'x-attribsNamespace': [Object: null prototype],
+//                                 'x-attribsPrefix': [Object: null prototype],
+//                                     children: [Array],
+//                                         parent: [Object],
+//                                             prev: [Object],
+//                                                 next: [Object]
+//         },
+//         prev: {
+//             type: 'text',
+//                 data: '\n\t\t\t\t\t\t\t',
+//                     parent: [Object],
+//                         prev: null,
+//                             next: [Circular]
+//         },
+//         next: {
+//             type: 'text',
+//                 data: '\n\t  \t\t\t\t',
+//                     parent: [Object],
+//                         prev: [Circular],
+//                             next: [Object]
+//         }
+//     },
+//     prev: null,
+//         next: {
+//         type: 'tag',
+//             name: 'img',
+//                 namespace: 'http://www.w3.org/1999/xhtml',
+//                     attribs: [Object: null prototype] {
+//             class: 'ingredients-list__item__icon',
+//                 src: 'https://assets.afcdn.com/recipe/20171128/75634_w100h100c1cx1250cy1250cxt0cyt0cxb2500cyb2500.jpg'
+//         },
+//         'x-attribsNamespace': [Object: null prototype] { class: undefined, src: undefined },
+//         'x-attribsPrefix': [Object: null prototype] { class: undefined, src: undefined },
+//         children: [],
+//             parent: {
+//             type: 'tag',
+//                 name: 'li',
+//                     namespace: 'http://www.w3.org/1999/xhtml',
+//                         attribs: [Object: null prototype],
+//                             'x-attribsNamespace': [Object: null prototype],
+//                                 'x-attribsPrefix': [Object: null prototype],
+//                                     children: [Array],
+//                                         parent: [Object],
+//                                             prev: [Object],
+//                                                 next: [Object]
+//         },
+//         prev: [Circular],
+//             next: {
+//             type: 'tag',
+//                 name: 'div',
+//                     namespace: 'http://www.w3.org/1999/xhtml',
+//                         attribs: [Object: null prototype] { },
+//             'x-attribsNamespace': [Object: null prototype] { },
+//             'x-attribsPrefix': [Object: null prototype] { },
+//             children: [Array],
+//                 parent: [Object],
+//                     prev: [Circular],
+//                         next: [Object]
+//         }
+//     }
+// }
